677c3c8b6a96f0470f7b2cbcc3b1e65baee8d6fe
volo_mori_9032 <scarycat105@gmail.com>
2026-07-11 23:21:24 +1200

    Initial commit

---
+++ src/main/java/net/scarycat105/theinbetween/TheInbetween.java
@@ -0,0 +1,22 @@
+package net.scarycat105.theinbetween;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheInbetween implements ModInitializer {
+	public static final String MOD_ID = "theinbetween";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
---
+++ src/main/java/net/scarycat105/theinbetween/TheInbetweenClient.java
@@ -0,0 +1,10 @@
+package net.scarycat105.theinbetween;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheInbetweenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
